5f28995007b7e280deaa705ec82364e5107ccf8b
Victor <vquesnel@student.42.fr>
2017-05-05 10:58:30 +0200

    Indentation

--- src/vendor/payline/index.js
+++ src/vendor/payline/index.js
@@ -1,8 +1,10 @@
 const Payline = require('payline');
 const config  = require('../../config');
 
-const payline = new Payline(config.PAYLINE_MERCHANT_ID,
-                  config.PAYLINE_ACCESS_KEY,
-                  config.PAYLINE_CONTRACT_NUMBER);
+const payline = new Payline(
+  config.PAYLINE_MERCHANT_ID,
+  config.PAYLINE_ACCESS_KEY,
+  config.PAYLINE_CONTRACT_NUMBER
+);
 
 module.exports = payline;
