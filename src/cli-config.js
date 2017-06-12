ac3616a324771299be3bd09c72e921be4ff37f84
Louis-Rémi Babé <lrbabe@gmail.com>
2017-06-12 16:31:02 +0200

    Fix config-cli.js

--- src/cli-config.js
+++ src/cli-config.js
@@ -1,11 +1,8 @@
 // The config must be exposed differently for sequelize-cli
 const config = require('./config');
 
-// We always return a config file with only the 'development' environment
-// (the default value for sequelize-cli), as environment selection happens
-// automatically in config.js for us.
 module.exports = {
-  development: {
+  [config.NODE_ENV]: {
     username: config.SEQUELIZE_USERNAME,
     password: config.SEQUELIZE_PASSWORD,
     database: config.SEQUELIZE_DATABASE,
