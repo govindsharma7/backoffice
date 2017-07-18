554004ca8c79d9c6dd49fa7833b8b423fe33bd78
Louis-Rémi Babé <lrbabe@gmail.com>
2017-07-18 15:00:35 +0200

    Fix linting error

--- scripts/updateLateFees.js
+++ scripts/updateLateFees.js
@@ -4,23 +4,21 @@ const models = require('../src/models');
 
 const {Order, Client} = models;
 
-var clients = [];
-
 return Order.scope('rentOrders')
   .findAll({
-  where: {dueDate: {$lt: Date.now()}},
-  include: [{model: Client}],
+    where: {dueDate: {$lt: Date.now()}},
+    include: [{model: Client}],
   })
   .map((order) => {
-      return Ninja.invoice.getInvoice({
-        'invoice_id': order.ninjaId,
-      })
-      .then((response) => {
-        const {data} = response.obj;
+    return Ninja.invoice.getInvoice({
+      'invoice_id': order.ninjaId,
+    })
+    .then((response) => {
+      const {data} = response.obj;
 
-        if ( data.invoice_status_id === Ninja.INVOICE_STATUS_PAID ) {
-          throw new Error('this invoice has been paid');
-        }
-        return order.Client.applyLateFees();
-      });
+      if ( data.invoice_status_id === Ninja.INVOICE_STATUS_PAID ) {
+        throw new Error('this invoice has been paid');
+      }
+      return order.Client.applyLateFees();
+    });
   });
