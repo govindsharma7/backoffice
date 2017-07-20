05c9958a46def5a5fc3d2bbfbe46536870509de0
Victor <vquesnel@student.42.fr>
2017-07-20 18:08:50 +0200

    modify script to get all rooms' pictures

--- scripts/extractWordpressPictures.js
+++ scripts/extractWordpressPictures.js
@@ -7,37 +7,24 @@ const rooms     = require('../data/rooms.json');
 const pictureRegex = /data-src="(.*?)"/g;
 var pictures = {};
 
-Promise.map(rooms.records, (room) => {
+return Promise
+  .map(rooms.records, (room) => {
     return Promise.all([
-      room.url,
+      fetch(room.url).then((r) => { return r.text(); }),
       room.reference,
     ]);
-  })
-  .then((urls) => {
-    return Promise.mapSeries(urls, (url) => {
-      return Promise.all([
-        fetch(url[0]),
-        url[1],
-      ]);
-    });
-  })
-  .then((responses) => {
-    return Promise.mapSeries(responses, (response) => {
-      return Promise.all([
-        response[0].text(),
-        response[1],
-      ]);
-    });
-  })
-  .then((htmls) => {
-    return Promise.mapSeries(htmls, (html) => {
-      let url;
+  }, {concurrency: 10})
+  .map(([html, reference]) => {
+    let url;
 
-      pictures[html[1]] = [];
-      while ( url = pictureRegex.exec(html[0]) ) {
-        pictures[html[1]].push(url[1]);
-      }
-      console.log(pictures);
-      return true;
-    });
+    pictures[reference] = [];
+    while ( url = pictureRegex.exec(html) ) {
+      pictures[reference].push(
+        url[1].replace('http://chez-nestor.com/wp-content/uploads', '')
+      );
+    }
+    return true;
+  })
+  .then(() => {
+    return console.log(JSON.stringify(pictures, null, '  '));
   });
