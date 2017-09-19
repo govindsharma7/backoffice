5f5d82a1d17478a154a46c0d8112e9199903a0e6
Louis-Rémi Babé <lrbabe@gmail.com>
2017-09-19 11:40:19 +0200

    Commit new files

---
+++ scripts/mysqlcopy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+docker run -it --rm mariadb mysqldump\
+  --host=$SEQUELIZE_HOST --user=$SEQUELIZE_USERNAME --password=$SEQUELIZE_PASSWORD\
+  backoffice > tmp/backoffice.sql\
+&&\
+docker run -it --rm mariadb mysqldump\
+  --host=$SEQUELIZE_HOST --user=$SEQUELIZE_USERNAME --password=$SEQUELIZE_PASSWORD\
+  ninja > tmp/ninja.sql\
+&&\
+docker run -i --rm mariadb mysql\
+  --host=$SEQUELIZE_HOST --user=$SEQUELIZE_USERNAME --password=$SEQUELIZE_PASSWORD\
+  backoffice-staging < tmp/backoffice.sql\
+&&\
+docker run -i --rm mariadb mysql\
+  --host=$SEQUELIZE_HOST --user=$SEQUELIZE_USERNAME --password=$SEQUELIZE_PASSWORD\
+   ninja-staging < tmp/ninja.sql;
