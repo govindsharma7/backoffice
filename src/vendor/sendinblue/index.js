9b4e7814ef82cb36702d85821389d29699e4a12b
Victor <vquesnel@student.42.fr>
2017-09-20 15:55:47 +0200

    remove console.log

--- src/vendor/sendinblue/index.js
+++ src/vendor/sendinblue/index.js
@@ -4,7 +4,6 @@ const {
   SUPPORT_EMAIL,
 }                   = require('../../const');
 
-
 const defaultClient = SendinBlueApi.ApiClient.instance;
 const apiKey = defaultClient.authentications['api-key'];
 
@@ -12,20 +11,19 @@ apiKey.apiKey = config.SENDINBLUE_API_KEY;
 
 const SMTPApi = new SendinBlueApi.SMTPApi();
 
-
 let commonData = {
   replyTo: SUPPORT_EMAIL,
 };
 
 function sendEmail(id, data = {}) {
   const email = Object.assign({}, commonData, data);
-console.log(email);
-//  if (email.emailTo.length > 0) {
-//  return SMTPApi.sendTemplate(id, email)
-//    .then(() => {
-//      return true ;
-//    });
-//  }
+
+  if (email.emailTo.length > 0) {
+    return SMTPApi.sendTemplate(id, email)
+      .then(() => {
+        return true ;
+      });
+  }
 
   return true;
 }
