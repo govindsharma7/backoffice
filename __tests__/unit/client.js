ab96dd98358401b3ab8bc356d29cc60c0625aec0
Victor <vquesnel@student.42.fr>
2017-09-25 16:41:20 +0200

    add future debit smart action

--- __tests__/unit/client.js
+++ __tests__/unit/client.js
@@ -122,11 +122,10 @@ describe('Client', () => {
           );
         })
         .then(([order, isCreated]) => {
-        console.log(order.OrderItems.length);
           return Promise.all([
             order.reload(),
             isCreated,
-            ]);
+          ]);
         })
         .then(([order, isCreated]) => {
           return (
