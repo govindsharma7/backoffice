d975f3c80c10518884bce67fd1052b7ef8cb02ff
Louis-Rémi Babé <lrbabe@gmail.com>
2017-09-26 15:10:58 +0200

    Make sure we don't add real emails to our sendinblue test account

--- src/vendor/sendinblue.js
+++ src/vendor/sendinblue.js
@@ -4,6 +4,7 @@ const {
   SUPPORT_EMAIL,
   SENDINBLUE_LIST_IDS,
 }                   = require('../const');
+const { NODE_ENV }  = require('../config');
 
 SendinBlueApi.ApiClient.instance.authentications['api-key'].apiKey =
   config.SENDINBLUE_API_KEY;
@@ -39,7 +40,12 @@ function getContact(email) {
 
 function createContact(email, {client, listIds}) {
   return ContactsApi.createContact({
-    email,
+    // In any environment but production, we always replace the email domain
+    // with our own, to make sure we never send an email to a real client
+    email: email.replace(
+      /@(.*)\.[^.]+$/,
+      NODE_ENV === 'production' ? '$0' : '_$1@chez-nestor.com'
+    ),
     attributes: serializeClient(client),
     listIds: listIds === null ?
       [SENDINBLUE_LIST_IDS.prospects[client.preferredLanguage]] : listIds,
