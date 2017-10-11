5c886bff1fd82a35493a5ce90fd038aa53a200fe
Louis-Rémi Babé <lrbabe@gmail.com>
2017-10-11 18:27:13 +0200

    Try to fix forest/sessions - 9

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ app.use(Cors({
 }));
 
 // Mime type
-app.use(BodyParser.json({limit: '6mb'}));
+// app.use(BodyParser.json({limit: '6mb'}));
 
 _.values(models).forEach(function(model) {
   if ('routes' in model) {
--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -1,7 +1,10 @@
  const config = require('../config');
 
 module.exports = function checkToken(req, res, next) {
-  if (req.query.secret_token === config.REST_API_SECRET) {
+  if (
+    ('secret_token' in req.query) &&
+    req.query.secret_token === config.REST_API_SECRET
+  ) {
     req.user = true;
   }
   next();
