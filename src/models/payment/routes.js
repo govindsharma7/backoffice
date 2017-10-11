064fd321ea34efa53be90dd8dcc52dc8bddefa56
Victor <vquesnel@student.42.fr>
2017-10-11 11:36:01 +0200

    fix rebase

--- src/models/payment/routes.js
+++ src/models/payment/routes.js
@@ -43,7 +43,7 @@ module.exports = function(app, models, Payment) {
 
     const rVisa = /^4[0-9]{12}(?:[0-9]{3})?$/;
     const rMastercard =
-/^(?:5[1-5][\d]{2}|222[1-9]|22[3-9][\d]|2[3-6][\d]{2}|27[01][\d]|2720)[\d]{12}$/;
+      /^(?:5[1-5][\d]{2}|222[1-9]|22[3-9][\d]|2[3-6][\d]{2}|27[01][\d]|2720)[\d]{12}$/;
     let type;
 
     if ( number.match(rVisa) ) {
@@ -55,21 +55,41 @@ module.exports = function(app, models, Payment) {
 
     Promise.resolve()
       .then(() => {
-        if ( !type) {
+        if ( !type ) {
           throw new Error('Invalid Card Type');
         }
 
-        return models.Order.scope('orderItems').findById(orderId);
+        return Promise.all([
+          models.Order.scope('orderItems').findById(orderId),
+          models.Order.scope('packItems').findById(orderId),
+        ]);
       })
-      .then((order) => {
-        if ( !order) {
+      .then(([order, packOrder]) => {
+        if ( !order ) {
           throw new Error(`Order "${orderId}" not found`);
         }
+
+        if ( packOrder ) {
+          /* eslint-disable promise/no-nesting */
+          return models.Room.scope('availableAt')
+            .findById(packOrder.OrderItems[0].Renting.RoomId)
+            .then((isAvailable) => {
+              if ( isAvailable && isAvailable.availableAt > Date.now() ) {
+                throw new Error('This room is no longer available.');
+              }
+              return order;
+            });
+          /* eslint-enable promise/no-nesting */
+        }
+
+        return order;
+      })
+      .then((order) => {
         return order.getCalculatedProps();
       })
       .then(({balance}) => {
-        if (balance === 0 ) {
-          throw new Error('Balance is null');
+        if (balance >= 0 ) {
+          throw new Error('Order is already fully paid.');
         }
 
         return Promise.all([
@@ -82,20 +102,27 @@ module.exports = function(app, models, Payment) {
               holder,
               cvx,
             },
-            Math.abs(balance)),
-          Math.abs(balance),
+            -balance
+          ),
+          -balance,
         ]);
       })
-      .then(([{transactionId}, amount]) => {
-        return models.Payment
-          .create({
-            type: 'card',
-            amount,
-            paylineId: transactionId,
-            OrderId: orderId,
-          });
+      .then(([{ transactionId }, amount]) => {
+        return Promise.all([
+          models.Payment
+            .create({
+              type: 'card',
+              amount,
+              paylineId: transactionId,
+              OrderId: orderId,
+            }),
+          models.Order.scope('packItems').findById(orderId),
+        ]);
       })
-      .then((payment) => {
+      .then(([payment, packOrder]) => {
+        if ( packOrder ) {
+          packOrder.markAsPaid();
+        }
         return res.send({paymentId: payment.id});
       })
       .catch(Utils.logAndSend(res));
