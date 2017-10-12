6a36298352630450fc0af3552b7a99a85c87d24e
Victor <vquesnel@student.42.fr>
2017-10-12 14:45:53 +0200

    scripts to send email depending on dueDate

---
+++ scripts/dueDateDayReminder.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+const Promise    = require('bluebird');
+const D          = require('date-fns');
+const fr         = require('date-fns/locale/fr');
+const models     = require('../src/models');
+const SendinBlue = require('../src/vendor/sendinblue');
+const {
+  SENDINBLUE_TEMPLATE_IDS,
+  PAYMENT_URL,
+}                = require('../src/config');
+
+const { Order } = models;
+
+return Order.scope('rentOrders')
+  .findAll({
+    where: {
+        'dueDate': new Date(),
+    },
+  })
+  .filter((order) => {
+  console.log(order);
+    return order.getCalculatedProps()
+      .then(({balance}) => {
+      return balance < 0;
+    });
+  })
+  .map((order) => {
+    return Promise.all([
+      order.getClient(),
+      order.getCalculatedProps(),
+      ])
+      .then(([client, {amount}]) => {
+        const lang = client.preferredLanguage === 'en' ? 'en-US' : 'fr-FR';
+
+        return SendinBlue.sendEmail(
+          SENDINBLUE_TEMPLATE_IDS.unpaidRent[client.preferredLanguage],
+          {
+            emailTo: [client.email],
+            attributes: {
+              FIRSTNAME: client.firstName,
+              MONTH: client.preferredLanguage === 'en' ?
+                D.format(order.dueDate, 'MMMM') :
+                D.format(order.dueDate, 'MMMM', {locale: fr}),
+              AMOUNT: amount / 100,
+              LINK: `${PAYMENT_URL}/${lang}/payment/${order.id}`,
+            },
+        });
+      });
+  })
+  .then(() => {
+    return process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
--- scripts/updateLateFees.js
+++ scripts/updateLateFees.js
@@ -1,24 +1,53 @@
 #!/usr/bin/env node
-const Ninja   = require('../src/vendor/invoiceninja');
-const models = require('../src/models');
+const Promise    = require('bluebird');
+const D          = require('date-fns');
+const fr         = require('date-fns/locale/fr');
+const models     = require('../src/models');
+const SendinBlue = require('../src/vendor/sendinblue');
+const {
+  SENDINBLUE_TEMPLATE_IDS,
+  PAYMENT_URL,
+}                = require('../src/config');
 
-const {Order, Client} = models;
+const { Client } = models;
 
-return Order.scope('rentOrders')
-  .findAll({
-    where: {dueDate: {$lt: Date.now()}},
-    include: [{model: Client}],
-  })
-  .map((order) => {
-    return Ninja.invoice.getInvoice({
-      'invoice_id': order.ninjaId,
-    })
-    .then((response) => {
-      const {data} = response.obj;
+return Client
+  .findAll()
+  .map((client) => {
+    return client.applyLateFees()
+      .map((order) => {
+        return Promise.all([
+          order.getCalculatedProps(),
+          models.OrderItem.findAll({
+            where: {
+              OrderId: order.id,
+              ProductId: 'late-fees',
+            },
+          }),
+        ])
+        .then(([{amount}, orderItems]) => {
+          const lang = client.preferredLanguage === 'en' ? 'en-US' : 'fr-FR';
 
-      if ( data.invoice_status_id === Ninja.INVOICE_STATUS_PAID ) {
-        throw new Error('this invoice has been paid');
-      }
-      return order.Client.applyLateFees();
+          return SendinBlue.sendEmail(
+            SENDINBLUE_TEMPLATE_IDS.lateFees[client.preferredLanguage],
+            {
+              emailTo: [client.email],
+              attributes: {
+                FIRSTNAME: client.firstName,
+                MONTH: client.preferredLanguage === 'en' ?
+                D.format(order.dueDate, 'MMMM') :
+                D.format(order.dueDate, 'MMMM', {locale: fr}),
+                AMOUNT: amount / 100,
+                LATE_FEES: orderItems[0].unitPrice * orderItems[0].quantity / 100,
+                LINK: `${PAYMENT_URL}/${lang}/payment/${order.id}`,
+              },
+          });
+        });
     });
+  })
+  .then(() => {
+    return process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
   });
