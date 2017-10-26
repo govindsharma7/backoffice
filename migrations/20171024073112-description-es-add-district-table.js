5c34118172b37bdf48f25ce9cf195db7f2c8eef6
Victor <vquesnel@student.42.fr>
2017-10-26 14:12:35 +0200

    route to upload or delete pictures

--- migrations/20171024073112-description-es-add-district-table.js
+++ migrations/20171024073112-description-es-add-district-table.js
@@ -2,49 +2,51 @@ const Promise = require('bluebird');
 
 module.exports = {
   up(queryInterface, Sequelize) {
-    return Promise.resolve()
-      .then(() => {
-        return Promise.all([
-          ['Apartment', 'Room'].forEach((table) => {
-            queryInterface.addColumn(
-              table,
-              'descriptionEs',
-              Sequelize.TEXT
-          );
+   return Promise.resolve()
+    .then(() => {
+      return Promise.all([
+        queryInterface.addColumn(
+          'Room',
+          'descriptionEs',
+          Sequelize.TEXT
+        ),
+        queryInterface.addColumn(
+          'Apartment',
+          'descriptionEs',
+          Sequelize.TEXT
+        ),
+        queryInterface.createTable(
+          'District',
+          {
+            id: {
+              type: Sequelize.STRING,
+              primaryKey: true,
+            },
+            createdAt: {
+              type: Sequelize.DATE,
+            },
+            updatedAt: {
+              type: Sequelize.DATE,
+            },
+            label: Sequelize.STRING,
+            descriptionFr: Sequelize.TEXT,
+            descriptionEn: Sequelize.TEXT,
+            descriptionEs: Sequelize.TEXT,
           }),
-          queryInterface.createTable(
-            'District',
-            {
-              id: {
-                type: Sequelize.STRING,
-                primaryKey: true,
-              },
-              createdAt: {
-                type: Sequelize.DATE,
-              },
-              updatedAt: {
-                type: Sequelize.DATE,
-              },
-              label: Sequelize.STRING,
-              descriptionFr: Sequelize.TEXT,
-              descriptionEn: Sequelize.TEXT,
-              descriptionEs: Sequelize.TEXT,
-            }),
-          queryInterface.removeColumn('Apartment', 'district'),
-        ]);
+     ]);
     })
     .then(() => {
       return queryInterface.sequelize.query(
         'ALTER TABLE Apartment ADD COLUMN DistrictId STRING REFERENCES District(id);');
     });
   },
-  down (queryInterface) {
+  down(queryInterface, Sequelize) {
     return Promise.all([
       queryInterface.dropTable('District'),
       ['Apartment', 'Room'].forEach((table) => {
         queryInterface.removeColumn(table, 'descriptionEs');
       }),
-      queryInterface.addColumn('Apartment', 'district'),
+      queryInterface.addColumn('Apartment', 'district', Sequelize.STRING),
       queryInterface.removeColumn('Apartment', 'DistrictId'),
     ]);
   },
