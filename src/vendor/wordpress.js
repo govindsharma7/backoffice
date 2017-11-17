7ecad390ce69da8846e9950ded5a1fe5e82a6a01
Louis-Rémi Babé <lrbabe@gmail.com>
2017-11-17 18:07:12 +0100

    Disable Wordpress connection

--- src/vendor/wordpress.js
+++ src/vendor/wordpress.js
@@ -2,11 +2,12 @@ const { required }    = require('../utils');
 const {
   WORDPRESS_AJAX_URL,
   REST_API_SECRET,
+  NODE_ENV,
 }                     = require('../config');
 const fetch           = require('./fetch');
 
 function updateRoomAvailability({ room = required() }) {
-  return fetch(WORDPRESS_AJAX_URL, {
+  return false && NODE_ENV === 'production' && fetch(WORDPRESS_AJAX_URL, {
     method: 'post',
     body: {
       action: 'update_availability',
